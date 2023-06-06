626aad7431236bc847b2bd00237892de66d89445
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-06 21:39:05 +0600

    Navbar Done

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../../Shared/Navbar/Navbar';
+import Footer from '../../Shared/Footer/Footer';
 
 const HomePage = () => {
     return (
         <div>
-            <h2>HomePage</h2>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
