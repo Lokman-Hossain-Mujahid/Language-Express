34e2c232226bfb9b215cc41a54db06a304900e14
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-06 21:47:08 +0600

    Auth provider done

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
     return (
         <div>
             <div>
-                <div className="navbar bg-base-100 text-2xl md:text-3xl">
+                <div className="navbar bg-orange-400 text-2xl md:text-3xl">
                     <div className="navbar-start">
                         <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <label tabIndex={0} className="pt-1 btn btn-ghost lg:hidden">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
                             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
@@ -30,15 +30,15 @@ const Navbar = () => {
                         </div>
                         <div className='flex items-center'>
                             <div>
-                                <img className='w-20' src="/public/images/logo.png" alt="" />
+                                <img className='w-20 pt-3' src="/public/images/logo.png" alt="" />
                             </div>
                             <div>
-                                <Link to="/" className="normal-case text-4xl font-semibold italic text-orange-500">LanguageExpress</Link>
+                                <Link to="/" className="normal-case md:text-3xl font-semibold italic text-white">LanguageExpress</Link>
                             </div>
                         </div>
                     </div>
-                    <div className="navbar-center hidden font-bebas lg:flex">
-                        <ul className="menu menu-horizontal px-1 text-2xl">
+                    <div className="navbar-center hidden font-bebas lg:flex ">
+                        <ul className="menu menu-horizontal px-1 text-3xl text-white">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/instructors">Instructors</Link></li>
                             <li><Link to="/classes">classes</Link></li>
