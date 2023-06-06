39f15bd2373b43ff4671a7cb71b35c1c017e0bdb
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-07 01:49:04 +0600

    Private Route Done

---
+++ src/Pages/InstructorsPage/InstructorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorPage = () => {
+    return (
+        <div>
+            <h2>Instructors</h2>
+        </div>
+    );
+};
+
+export default InstructorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,30 +4,36 @@ import HomeDetails from "../components/HomeDetails/HomeDetails";
 import Login from "../Auth/Login/Login";
 import Register from "../Auth/Register/Register";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import InstructorPage from "../Pages/InstructorsPage/InstructorPage";
+import PrivateRoute from "./PrivateRoute";
 
 
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage></HomePage>,
-      errorElement: <ErrorPage></ErrorPage>,
-      children: [
-        {
-            path: "/",
-            element: <HomeDetails></HomeDetails>
-        }
-      ]
-    },
-    {
-        path: "/login",
-        element: <Login></Login>
-    },
-    {
-      path: '/register',
-      element: <Register></Register>
-    }
-  ]);
+  {
+    path: "/",
+    element: <HomePage></HomePage>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <HomeDetails></HomeDetails>
+      },
+      {
+        path: "/instructors",
+        element: <PrivateRoute><InstructorPage></InstructorPage></PrivateRoute>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login></Login>
+  },
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+]);
 
 
 export default router;
