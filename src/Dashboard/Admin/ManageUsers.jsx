707488fd0c09332e2b02ee5eb6246c198ea5ac59
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-08 04:32:58 +0600

    having issues with role update

--- src/Dashboard/Admin/ManageUsers.jsx
+++ src/Dashboard/Admin/ManageUsers.jsx
@@ -1,92 +1,131 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { FaUserShield, FaChalkboardTeacher } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaUserShield, FaChalkboardTeacher } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
+  const { data: users = [], refetch } = useQuery(['users'], async () => {
+    const res = await fetch('http://localhost:5000/users');
+    return res.json();
+  });
 
-    const { data: users = [], refetch } = useQuery(['users'], async () => {
-        const res = await fetch('http://localhost:5000/users')
-        return res.json()
+  const [disabledInstructorUsers, setDisabledInstructorUsers] = useState([]);
+  const [disabledAdminUsers, setDisabledAdminUsers] = useState([]);
+
+  const handleMakeAdmin = user => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: 'PATCH'
     })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          setDisabledAdminUsers([
+            ...disabledAdminUsers.filter(id => id !== user._id),
+            user._id
+          ]);
+          setDisabledInstructorUsers([
+            ...disabledInstructorUsers.filter(id => id !== user._id)
+          ]);
+          Swal.fire({
+            position: 'top',
+            icon: 'success',
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 2000
+          });
+        }
+      });
+  };
 
-    const handleMakeAdmin = user => {
-        fetch(`http://localhost:5000/users/admin/${user._id}`, {
-            method: 'PATCH'
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.modifiedCount) {
-                    refetch()
-                    Swal.fire({
-                        position: 'top',
-                        icon: 'success',
-                        title: `${user.name} is an Admin Now!`,
-                        showConfirmButton: false,
-                        timer: 2000
-                    });
-                }
-            })
-    }
+  const handleMakeInstructor = user => {
+    fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+      method: 'PATCH'
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          setDisabledInstructorUsers([
+            ...disabledInstructorUsers.filter(id => id !== user._id),
+            user._id
+          ]);
+          setDisabledAdminUsers([
+            ...disabledAdminUsers.filter(id => id !== user._id)
+          ]);
+          Swal.fire({
+            position: 'top',
+            icon: 'success',
+            title: `${user.name} is an Instructor Now!`,
+            showConfirmButton: false,
+            timer: 2000
+          });
+        }
+      });
+  };
 
-    const handleMakeInstructor = user => {
-        fetch(`http://localhost:5000/users/instructor/${user._id}`, {
-            method: 'PATCH'
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.modifiedCount) {
-                    refetch()
-                    Swal.fire({
-                        position: 'top',
-                        icon: 'success',
-                        title: `${user.name} is an Instructor Now!`,
-                        showConfirmButton: false,
-                        timer: 2000
-                    });
-                }
-            })
-    }
-
-
-    return (
-        <div className='w-full'>
-
-            <div className="overflow-x-auto">
-                <table className="table table-zebra">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Make Instructor</th>
-                            <th>Make Admin</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {
-                            users.filter(user => user.email !== 'rclash163@gmail.com' ).map((user, index) =>
-                                <tr key={index}>
-                                    <th>{index + 1}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.role}</td>
-                                    <td>{user.role === 'instructor' ? <button className="btn btn-warning disabled"><FaChalkboardTeacher></FaChalkboardTeacher></button> : <button onClick={() => handleMakeInstructor(user)} className="btn btn-warning"><FaChalkboardTeacher></FaChalkboardTeacher></button>}
-                                    </td>
-                                    <td>{user.role === 'admin' ? <button className="btn btn-warning disabled"><FaUserShield></FaUserShield></button> : <button onClick={() => handleMakeAdmin(user)} className="btn btn-warning"><FaUserShield></FaUserShield></button>}</td>
-                                </tr>)
-                        }
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Make Instructor</th>
+              <th>Make Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users
+              .filter(user => user.email !== 'rclash163@gmail.com')
+              .map((user, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    {user.role === 'instructor' ? (
+                      <button className="btn btn-warning disabled">
+                        <FaChalkboardTeacher />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeInstructor(user)}
+                        className="btn btn-warning"
+                        disabled={disabledInstructorUsers.includes(user._id)}
+                      >
+                        <FaChalkboardTeacher />
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    {user.role === 'admin' ? (
+                      <button className="btn btn-warning disabled">
+                        <FaUserShield />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-warning"
+                        disabled={disabledAdminUsers.includes(user._id)}
+                      >
+                        <FaUserShield />
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageUsers;
