3611e153860042f26e91c839f28554b18c6d9f58
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-07 15:10:09 +0600

    Instructor Role Implemented

--- src/Dashboard/Admin/ManageUsers.jsx
+++ src/Dashboard/Admin/ManageUsers.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { FaUserShield, FaChalkboardTeacher } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
 
@@ -8,8 +10,49 @@ const ManageUsers = () => {
         return res.json()
     })
 
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: 'top',
+                        icon: 'success',
+                        title: `${user.name} is an Admin Now!`,
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
+    }
+
+    const handleMakeInstructor = user => {
+        fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: 'top',
+                        icon: 'success',
+                        title: `${user.name} is an Instructor Now!`,
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
+    }
+
+
     return (
-        <div>
+        <div className='w-full'>
             <div>
                 <h2>{users.length}</h2>
             </div>
@@ -18,34 +61,29 @@ const ManageUsers = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>#</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Make Instructor</th>
+                            <th>Make Admin</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+
+                        {
+                            users.map((user, index) =>
+                                <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.role}</td>
+                                    <td>{user.role === 'instructor' ? <button className="btn btn-warning disabled"><FaChalkboardTeacher></FaChalkboardTeacher></button> : <button onClick={() => handleMakeInstructor(user)} className="btn btn-warning"><FaChalkboardTeacher></FaChalkboardTeacher></button>}
+                                    </td>
+                                    <td>{user.role === 'admin' ? <button className="btn btn-warning disabled"><FaUserShield></FaUserShield></button> : <button onClick={() => handleMakeAdmin(user)} className="btn btn-warning"><FaUserShield></FaUserShield></button>}</td>
+                                </tr>)
+                        }
+
                     </tbody>
                 </table>
             </div>
