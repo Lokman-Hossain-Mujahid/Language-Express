dad2800788b1f46b47f11e50a06ae193c6fe3017
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-09 00:33:53 +0600

    Admin Manage Classes Panel Done

--- src/Dashboard/Admin/ManageClasses.jsx
+++ src/Dashboard/Admin/ManageClasses.jsx
@@ -1,25 +1,116 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+import { useQuery } from '@tanstack/react-query';
 
 const ManageClasses = () => {
 
+    // FEEDBACK MODAL PART
 
-    const [addedClasses, setAddedClasses] = useState([])
+    const [modalOpen, setModalOpen] = useState(false);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/classes')
-            .then(res => res.json())
-            .then(data => {
+
+    const handleGetFeedback = (event) => {
+        event.preventDefault();
+    
+        const form = event.target;
+        const feedBack = form.feedBack.value;
+        const id = form.id.value;
+        console.log(feedBack);
+        fetch(`http://localhost:5000/feedback/${id}`, {
+          method: 'PUT',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({ feedBack: feedBack }),
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            if (result.modifiedCount > 0) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Feedback Submitted',
+                text: 'Your feedback has been submitted successfully.',
+                confirmButtonText: 'OK',
+                onClose: () => {
+                  // Handle any necessary actions after the alert is closed
+                  reset(); // Reset the form fields
+                  setModalOpen(false); // Close the modal
+                },
+              });
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+    
+
+
+    // DATA FETCHING
+
+    const { data: addedClasses = [], refetch } = useQuery(
+        ['addedClasses'],
+        async () => {
+            const res = await fetch('http://localhost:5000/classes');
+            return res.json();
+        }
+    );
+
+
+    // HANDLE APPROVE
+    const handleApprove = (addClass) => {
+        fetch(`http://localhost:5000/classes/${addClass._id}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ status: 'approved' }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
                 console.log(data);
-                setAddedClasses(data)
-            })
-    }, [])
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top',
+                        icon: 'success',
+                        title: `${addClass.className} is Approved!`,
+                        showConfirmButton: false,
+                        timer: 2000,
+                    });
+                }
+            });
+    };
+
+
+    // HANDLE DENY
+    const handleDeny = (addClass) => {
+        fetch(`http://localhost:5000/classes/${addClass._id}`, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ status: 'denied' }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top',
+                        icon: 'success',
+                        title: `${addClass.className} is Denied!`,
+                        showConfirmButton: false,
+                        timer: 2000,
+                    });
+                }
+            });
+    };
 
     return (
         <div className="w-full">
             <div className="overflow-x-auto">
                 <table className="table table-zebra">
                     <thead>
-                        <tr className=''>
+                        <tr className="text-center">
                             <th>#</th>
                             <th>Class Image</th>
                             <th>Class Name</th>
@@ -29,41 +120,67 @@ const ManageClasses = () => {
                             <th>Price</th>
                             <th>Status</th>
                             <th>Actions</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* {
-                            addedClasses.map((addClass, index) => console.log(addClass))
-                        } */}
-                        {
-                            addedClasses.map((addClass, index) => (
-                            
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src={addClass.image} alt="Avatar Tailwind CSS Component" />
+                        {addedClasses.map((addClass, index) => (
+                            <tr className="text-center" key={index}>
+                                <td>{index + 1}</td>
+                                <td className="flex justify-center">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={addClass.image} alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </td>
+                                <td>{addClass.className}</td>
+                                <td>{addClass.instructorName}</td>
+                                <td>{addClass.instructorEmail}</td>
+                                <td>{addClass.availableSeats}</td>
+                                <td>${addClass.price}</td>
+                                <td>{addClass.status}</td>
+                                <td className="">
+                                    <button onClick={() => handleApprove(addClass)} className="btn btn-warning">
+                                        Approve
+                                    </button>
+                                    <button onClick={() => handleDeny(addClass)} className="btn btn-warning mx-1">
+                                        Deny
+                                    </button>
+                                </td>
+                                <td>
+                                    <label htmlFor={addClass._id} className="btn">
+                                        Feedback
+                                    </label>
+                                    <input
+                                        type="checkbox"
+                                        id={addClass._id}
+                                        className="modal-toggle"
+                                    />
+                                    <div className="modal">
+                                        <div className="modal-box w-11/12 max-w-5xl">
+                                            <h2 className="text-6xl font-bebas font-semibold text-center">
+                                                Give Feedback
+                                            </h2>
+                                            <hr className="md:w-1/4 mx-auto border-yellow-500 mb-6" />
+                                            <div className="text-center md:my-6 md:h-[70vh] hero bg-orange-300 py-5 md:w-2/4 mx-auto rounded-lg">
+                                                <form className='flex flex-col' onSubmit={handleGetFeedback}>
+
+                                                    <textarea name='feedBack' defaultValue={addClass.feedBack
+                                                    } className="textarea my-2" placeholder="feedback"></textarea>
+                                                    <input name='id' type="text" value={addClass._id} />
+                                                    <input name='email' type="email" value={addClass.instructorEmail} />
+                                                    <input className='btn btn-primary my-2' type='submit'></input>
+                                                </form>
+                                            </div>
+                                            <div className="modal-action">
+                                                <label htmlFor={addClass._id} className="btn" onClick={() => setModalOpen(false)}>
+                                                    Done
+                                                </label>
+                                            </div>
                                         </div>
-                                    </td>
-                                    <td>{addClass.className}</td>
-                                    <td>{addClass.instructorName}</td>
-                                    <td>{addClass.instructorEmail}</td>
-                                    <td>{addClass.availableSeats}</td>
-                                    <td>${addClass.price}</td>
-                                    <td>{addClass.status}</td>
-                                    
-                                    <td className=''>
-
-                                        <button className='btn btn-warning'>1</button>
-                                        <button className='btn btn-warning'>2</button>
-                                        <button className='btn btn-warning'>3</button>
-                                    
-                                    </td>
-
-                                </tr>
-                            )
-                            )
-                        }
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
